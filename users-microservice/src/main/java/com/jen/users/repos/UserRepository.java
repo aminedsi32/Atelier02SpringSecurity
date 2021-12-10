52eafa72443446bdad96a9831c0bb6a730104911
aminedsi32 <93100993+aminedsi32@users.noreply.github.com>
2021-12-10 02:02:03 +0100

    Delete users-microservice directory

--- users-microservice/src/main/java/com/jen/users/repos/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jen.users.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.jen.users.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	User findByUsername(String username);
-}
